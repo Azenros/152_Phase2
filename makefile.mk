90987323ecdda17365fa8a630907422eb2fc35e7
JiHounHuh <25492217+JiHounHuh@users.noreply.github.com>
2020-02-19 12:25:26 -0800

    Updating makefile.mk to include other output files in build and clean

--- makefile.mk
+++ makefile.mk
@@ -5,7 +5,7 @@ OBJS    = bison.o lex.o main.o
 CC      = g++
 CFLAGS  = -g -Wall -ansi -pedantic
 
-calc:           $(OBJS)
+mini_l:           $(OBJS)
                 $(CC) $(CFLAGS) $(OBJS) -o calc -lfl
 
 lex.o:          lex.c
@@ -19,9 +19,9 @@ bison.o:        bison.c
                 $(CC) $(CFLAGS) -c bison.c -o bison.o
 
 bison.c:        mini_l.y
-                bison -d -v mini_l.y
-                cp mini_l.tab.c bison.c
-                cmp -s mini_l.tab.h tok.h || cp mini_l.tab.h tok.h
+                bison -d -v --file-prefix=y mini_l.y
+                cp y.tab.c bison.c
+                cmp -s y.tab.h tok.h || cp y.tab.h tok.h
 
 main.o:         main.cc
                 $(CC) $(CFLAGS) -c main.cc -o main.o
@@ -30,4 +30,4 @@ lex.o yac.o main.o      : heading.h
 lex.o main.o            : tok.h
 
 clean:
-        rm -f *.o *~ lex.c lex.yy.c bison.c tok.h mini_l.tab.c mini_l.tab.h mini_l.output mini_l
+        rm -f *.o *~ lex.c lex.yy.c bison.c tok.h y.tab.c y.tab.h y.output mini_l
