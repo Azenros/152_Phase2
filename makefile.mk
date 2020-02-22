7951987ff5b89dfd82de95e5f856ef0389a0a65c
JiHounHuh <25492217+JiHounHuh@users.noreply.github.com>
2020-02-21 19:06:26 -0800

    Update makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -1,6 +1,6 @@
 # Makefile
 
-OBJS	= bison.o lex.o main.o
+OBJS	= bison.o lex.o 
 
 CC	= g++
 CFLAGS	= -g -Wall -ansi -pedantic
@@ -19,15 +19,11 @@ bison.o:	bison.c
 		$(CC) $(CFLAGS) -c bison.c -o bison.o
 
 bison.c:	mini_l.y
-		bison -d -v mini_l.y
-		cp mini_l.tab.c bison.c
-		cmp -s mini_l.tab.h tok.h || cp mini_l.tab.h tok.h
+		bison -d -v --file-prefix=y mini_l.y
+		cp y.tab.c bison.c
 
 main.o:		main.cc
 		$(CC) $(CFLAGS) -c main.cc -o main.o
 
-lex.o yac.o main.o	: heading.h
-lex.o main.o		: tok.h
-
 clean:
-	rm -f *.o *~ lex.c lex.yy.c bison.c tok.h mini_l.tab.c mini_l.tab.h mini_l.output mini_l
+	rm -f *.o *~ lex.c lex.yy.c bison.c y.tab.c y.tab.h y.output mini_l
