ee325a3bf260c23ee59f2941fb7f1e8174b0d0ae
JiHounHuh <25492217+JiHounHuh@users.noreply.github.com>
2020-03-13 21:41:24 -0700

    Fix syntax

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ make
 cat $1 | mini_l > $1.mil
 echo $1.mil
 echo "Running mil_run on generated mil file..."
-if [ -z "$2" ]; then 
-  mil_run $1.min
+if [[ -z "$2" ]]
+then
+        mil_run $1.mil
 else
-  mil_run $1.min < $2
+        mil_run $1.mil < $2
 fi
-
